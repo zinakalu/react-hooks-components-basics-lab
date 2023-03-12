88c4d85e114669f4f932104a28815265da9b4a82
zinakalu <zina.kalu@gmail.com>
2023-03-12 10:20:27 -0400

    component basics lab

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+const About = ()=> {
+  return (
+<div id="about">
+  <h2>About</h2>
+</div>
+  );
+}
 
 
 function App() {
@@ -24,7 +31,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
